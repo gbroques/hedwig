aef4e01b8da72d5ef602d24d01598ca84ff2f1b9
G Brenden Roques <groques360@gmail.com>
2017-11-07 21:44:17 +0000

    Add migration model, and route for lessons (Closes #2)

---
+++ app/Http/Controllers/LessonController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lesson;
+use Illuminate\Http\Request;
+
+class LessonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Lesson::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Lesson::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lesson $lesson)
+    {
+        return Lesson::find($lesson->id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lesson $lesson)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lesson $lesson)
+    {
+        $lesson = Lesson::find($lesson->id);
+        $lesson->update($request->all());
+        return $lesson;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lesson $lesson)
+    {
+        return Lesson::destroy($lesson->id);
+    }
+}
