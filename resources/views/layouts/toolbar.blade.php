984f8baaf81ed3d5ff7a8eec14847e7c336512a9
G Roques <groques360@gmail.com>
2017-11-09 08:48:29 -0600

    Remove menu button

--- resources/views/layouts/toolbar.blade.php
+++ resources/views/layouts/toolbar.blade.php
@@ -1,8 +1,4 @@
 <md-toolbar>
-  <md-button class="md-icon-button">
-    <md-icon>menu</md-icon>
-  </md-button>
-
   <h2 class="md-title" style="flex: 1">Hedwig</h2>
 
   <md-button @click="$router.replace({ name: 'CourseCatalog' })">
