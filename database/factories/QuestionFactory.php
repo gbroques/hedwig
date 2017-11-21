9fd55c16b61c339a6ad4673d3efcacdbab4b89d0
G Roques <groques360@gmail.com>
2017-11-21 15:35:06 -0600

    Update factory with lesson id and youtube playlist id

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
     return [
-        'text' => $faker->text
+        'text' => $faker->text,
+        'youtube_playlist_item_id' => 'UExlc2lQNDl6RzZzbVkyZGt5MHlRZFo2ekpZT0dtMnNWZS41NkI0NEY2RDEwNTU3Q0M2',
+        'lesson_id' => 1
     ];
 });
