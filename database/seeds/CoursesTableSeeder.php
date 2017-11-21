433711a971a9910b16e6d234cad1dc69852307d6
G Roques <groques360@gmail.com>
2017-11-21 14:27:08 -0600

    Add course factory and seeder

---
+++ database/seeds/CoursesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Course;
+use Illuminate\Database\Seeder;
+
+class CoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Course::truncate();
+        factory(Course::class, 1)->create();
+    }
+}
