3f86a8f121627c6947df16f762663bacc47c9f6d
G Roques <groques360@gmail.com>
2017-11-30 08:32:07 -0600

    Remove private channel debug statements

--- resources/assets/js/echo.js
+++ resources/assets/js/echo.js
@@ -4,9 +4,3 @@ window.Echo = new Echo({
     broadcaster: 'socket.io',
     host: window.location.hostname + ':6001'
 });
-
-console.log('Echo', window.Echo);
-
-window.Echo.private('chat').listen('typing', (e) => {
-  console.log(e);
-})
